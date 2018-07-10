3ac4c59199dd3dcc29dc9f87a0b38c9c1e31426a
ctialitu <dgrdavtyan@mail.ru>
2018-07-10 18:03:31 +0400

    ctialitu

--- script.js
+++ script.js
@@ -1,72 +1,82 @@
 $(document).ready(function () {
-    $("#new_id").click(function(){
+    $("#new_id").click(function () {
         $("#new_id").toggleClass("clickMenuTow");
         $("#opening").toggleClass("opened");
     });
-    $("#nav2").click(function() {
+    $("#nav2").click(function () {
         $('html,body').animate({
-                scrollTop: $("#point1").offset().top},
+                scrollTop: $("#point1").offset().top
+            },
             'slow');
     });
-    $("#nav3").click(function() {
+    $("#nav3").click(function () {
         $('html,body').animate({
-                scrollTop: $("#point2").offset().top},
+                scrollTop: $("#point2").offset().top
+            },
             'slow');
     });
-    $("#nav4").click(function() {
+    $("#nav4").click(function () {
         $('html,body').animate({
-                scrollTop: $("#point3").offset().top},
+                scrollTop: $("#point3").offset().top
+            },
             'slow');
     });
-    $("#nav5").click(function() {
+    $("#nav5").click(function () {
         $('html,body').animate({
-                scrollTop: $("#point4").offset().top},
+                scrollTop: $("#point4").offset().top
+            },
             'slow');
     });
-    $("#nav6").click(function() {
+    $("#nav6").click(function () {
         $('html,body').animate({
-                scrollTop: $("#point5").offset().top},
+                scrollTop: $("#point5").offset().top
+            },
             'slow');
     });
-    $("#nav21").click(function() {
+    $("#nav21").click(function () {
         $('html,body').animate({
-                scrollTop: $("#point1").offset().top},
+                scrollTop: $("#point1").offset().top
+            },
             'slow');
     });
-    $("#nav31").click(function() {
+    $("#nav31").click(function () {
         $('html,body').animate({
-                scrollTop: $("#point1").offset().top},
+                scrollTop: $("#point2").offset().top
+            },
             'slow');
     });
-    $("#nav41").click(function() {
+    $("#nav41").click(function () {
         $('html,body').animate({
-                scrollTop: $("#point1").offset().top},
+                scrollTop: $("#point3").offset().top
+            },
             'slow');
     });
-    $("#nav51").click(function() {
+    $("#nav51").click(function () {
         $('html,body').animate({
-                scrollTop: $("#point1").offset().top},
+                scrollTop: $("#point4").offset().top
+            },
             'slow');
     });
-    $("#nav61").click(function() {
+    $("#nav61").click(function () {
         $('html,body').animate({
-                scrollTop: $("#point1").offset().top},
+                scrollTop: $("#point5").offset().top
+            },
             'slow');
     });
 
     $("#mail").blur(function () {
-       var string1 = $("#mail").val();
-       var string_mass = string1.match(/[a-z][a-z0-9\.]+[a-z]+\@[a-z]+\.[a-z]{2,}/ig);
-       if (string_mass !== null){
-       var string_filter = string_mass.join();
-       if (string_filter === string1){
-           $("#mail").removeClass('red');
-       }else {
-           $("#mail").addClass('red');
-       }
-       }else {
-           $("#mail").addClass('red');
-       }
+        var string1 = $("#mail").val();
+        var string_mass = string1.match(/[a-z][a-z0-9\.]+[a-z]+\@[a-z]+\.[a-z]{2,}/ig);
+        if (string_mass !== null) {
+            var string_filter = string_mass.join();
+            if (string_filter === string1) {
+                $("#mail").removeClass('red');
+            } else {
+                $("#mail").addClass('red');
+            }
+        } else {
+            $("#mail").addClass('red');
+        }
     });
     $("#mail").focus(function () {
         $("#mail").removeClass('red');
